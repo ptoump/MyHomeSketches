7b9cf27950d22c45a7dee1d78e19fd558889ff38
ptoump <ptoump.spm1@gmail.com>
2015-12-04 21:21:37 +0200

    Working Set 1

--- Battery_Meter/Battery_Meter.ino
+++ Battery_Meter/Battery_Meter.ino
@@ -1,21 +1,22 @@
-#include <Time.h>
+#include <Timer.h>
 #include <SPI.h>
 
 #define TIMESTEP 10         //define interval in seconds
-#define RESISTOR 4.2    	//define value of resistor used as shunt
-#define ENDVOLTAGE 2.9    	//define the voltage at which the test stops
-#define VOLTAGEPIN A1    		//define the pin number for input A, which will be used to read the cell positive voltage
-#define RELAY 12           	//this is the pin number of the output to the transistor base or mosfet gate        
+#define RESISTOR 4.5    	//define value of resistor used as shunt
+#define ENDVOLTAGE 2900    	//define the voltage at which the test stops in mV
+#define VOLTAGEPIN A1    	//define the pin number for Analog input
+#define RELAY 12           	//this is the pin number of the output to the Relay Board        
 
 float capacity =0;
-unsigned prevMillis = 0;
-unsigned currMillis = 0;
+long prevMillis = 0;
+long currMillis = 0;
 unsigned period = 0;
 float voltage = 4.2;
 float current = voltage / RESISTOR ;
 float stepCapacity = current * TIMESTEP ;
 float totalCapacity = 0 ;
 int timer_int = 0;
+float correction = 3810 / 4272.;  // Correction factor ( Multimeter / analog read )
 
 Timer t;
 
@@ -26,7 +27,7 @@ void setup()
   	Serial.println("Timestamp;Period;Voltage;Current;Capacity_in_Period;Total_Capacity");
   	prevMillis = millis(); 	
   	digitalWrite(RELAY, HIGH);
-  	timer_int = t.every( TIMESTEP * 1000L , doProcess)
+  	timer_int = t.every( TIMESTEP * 1000L , doProcess);
 }
 
 void loop()
@@ -39,13 +40,13 @@ void doProcess()
 {	
 	currMillis = millis();
 	period = currMillis - prevMillis ;
-	voltage = (float)(analogRead(VOLTAGEPIN)*5)/1024.);
+	voltage = (float)((analogRead(VOLTAGEPIN) * 5 * correction)/1.024);
 	prevMillis = currMillis;
 	current = voltage / RESISTOR ;
-	stepCapacity = (current * period) / 1000.;
+	stepCapacity = (current * period) / (3600 * 1000.);
 	totalCapacity += stepCapacity;
-	Serial.print(currMillis);Serial.print(";");
-	Serial.print(period);Serial.print(";");
+	Serial.print(currMillis / 1000);Serial.print(";");
+	Serial.print(period / 1000);Serial.print(";");
 	Serial.print(voltage);Serial.print(";");
 	Serial.print(current);Serial.print(";");
 	Serial.print(stepCapacity);Serial.print(";");
